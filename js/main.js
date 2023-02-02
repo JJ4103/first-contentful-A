ba43631f224780bf7c69bcb2f804341c81def4b8
JJ4103 <98718693+JJ4103@users.noreply.github.com>
2023-02-02 14:33:01 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 console.log('main.js loaded')
+
+var client = contrnful.createclient({
+    space: 'dqptk4h1t60x',
+    accesstoken: 'rDOYcxsI_dee6j8sMjf7EZmo1opoGDhzWayVCIrug7I',
+});
+console.log(client);
+
+client.getEntries().then(function(entries){
+    entries.items.forEach(function(entry){
+        if(entry.fields.title){
+            console.log(entry.fields.title);
+        }
+    })
+})
